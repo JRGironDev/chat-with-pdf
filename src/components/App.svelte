d54f6b838c57083281b8b37ff82ddfaa61f781a7
José Quiñónez <joserobertoq@outlook.com>
2024-08-29 06:30:17 -0600

    Add Selvet Status

---
+++ src/components/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { Alert, P } from "flowbite-svelte";
+  import { APP_STATUS, appStatus } from "../store.ts";
+  import StepUploading from "./StepUploading.svelte";
+  import StepLoading from "./StepLoading.svelte";
+  import StepChat from "./StepChat.svelte";
+</script>
+
+{#if $appStatus === APP_STATUS.INIT}
+  <StepUploading />
+{:else if $appStatus === APP_STATUS.LOADING}
+  <StepLoading />
+{:else if $appStatus === APP_STATUS.ERROR}
+  <Alert>
+    <span class="font-medium">Error!</span>
+    Hay un error en la aplicación
+  </Alert>
+{:else if $appStatus === APP_STATUS.CHAT_MODE}
+  <StepChat />
+{:else}
+  <Alert>
+    <span class="font-medium">Acción no reconocida</span>
+    Este estado no está reconocido
+  </Alert>
+{/if}
---
+++ src/components/StepLoading.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { Spinner } from "flowbite-svelte";
+</script>
+
+<div class="flex justify-center items-center flex-col gap-y-2">
+  <Spinner />
+  <p class="opacity-70">Cargando y extrayendo el contenido del pdf ...</p>
+</div>
